49ed2ca379056377604000f4d23542a67a42c440
Igor Primo <igorprimo62@gmail.com>
2025-12-31 22:24:59 -0300

    typo

--- NixSupport/nixosModules/services/app.nix
+++ NixSupport/nixosModules/services/app.nix
@@ -16,7 +16,7 @@ in
             KillSignal = "SIGINT";
             WatchdogSec = "60";
             Sockets = "app.socket";
-            serviceConfig.EnvironmentFile = cfg.environmentFilePath;
+            EnvironmentFile = cfg.environmentFilePath;
         };
         environment =
             let
